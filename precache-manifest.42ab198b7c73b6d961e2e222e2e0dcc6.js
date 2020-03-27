36629e5ef46c94401757740daeade3c834458f36
Theodoor Bongers <email@theodoorbongers.nl>
2020-03-27 23:17:47 +0100

    Updates

---
+++ precache-manifest.42ab198b7c73b6d961e2e222e2e0dcc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a7a68c5f1344e8a5c3ca9308ab9071",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a2d94ef6449fbc254e1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9cf7518ae94a7a92d14d",
+    "url": "/static/js/2.29501e45.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.29501e45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a2d94ef6449fbc254e1",
+    "url": "/static/js/main.d9f8b4d2.chunk.js"
+  },
+  {
+    "revision": "26faeb3a29364b7b52c5",
+    "url": "/static/js/runtime-main.13276886.js"
+  }
+]);
